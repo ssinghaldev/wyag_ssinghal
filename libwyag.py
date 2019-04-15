b7942a11164161eca0ae715018ba9900f330a67a
Shubham Singhal <sbmsgl1129@gmail.com>
2019-04-15 11:44:19 +0530

    project start  - import modules

---
+++ libwyag.py
@@ -0,0 +1,14 @@
+# This project we are writing our own git. 
+# Basically I am just implementing this : https://wyag.thb.lt/
+# Implementing is a 'big' word, just suplicating the above thing is more 
+# accurate description. But I will not just copy-paste. I will type out the commands.
+# I am confident I will learn a lot while doing this.
+
+import argparse
+import collections
+import configparser
+import hashlib
+import os
+import re
+import sys
+import zlib
