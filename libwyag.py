60a1374d45897c5c82602886f9ce3fd4c2cb8cac
Shubham Singhal <sbmsgl1129@gmail.com>
2019-05-09 10:45:54 +0530

    Add helper functions  - to manipulate repo_paths - to create default repo_config - to create a repo - will be used by 'init' command

--- libwyag.py
+++ libwyag.py
@@ -38,7 +38,72 @@ class GitRepository:
         if not force:
             version = int(self.config_parser.get("core", "repositoryformatversion"))
             if version != 0:
-                raise Exception("Unsupported repositoryformatversion %s" % version")
+                raise Exception("Unsupported repositoryformatversion %s" % version)
+
+def repo_path(repo, *path):
+    """Compute path under repo's gitdir"""
+    return os.path.join(repo.gitdir, *path)
+
+def repo_file(repo, *path, mkdir=False):
+    """Same as repo_path but create dirname(*path) if absent"""
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+    
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path, but mkdir *path if absent if mkdir"""
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+    
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+def repo_create(path):
+    """Create a new repository at path"""
+    repo = GitRepository(path, True)
+
+    #First we make sure that path doesn't exist or is an empty dir
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception("%s is not a directory!" % path)
+        if os.listdir(repo.worktree):
+            raise Exception("%s is not empty!" % path)
+    else:
+        os.makedirs(repo.worktree)
+    
+    #Next create all important dirs
+    assert(repo_dir(repo, "branches", mkdir=True))
+    assert(repo_dir(repo, "objects", mkdir=True))
+    assert(repo_dir(repo, "refs", "tags", mkdir=True))
+    assert(repo_dir(repo, "refs", "heads", mkdir=True))
+
+    #create all important files
+    with open(repo_path(repo, "description", "w")) as f:
+        f.write("Unnamed repository; edit this file 'description' to name the repository.\n")
+    with open(repo_path(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+    with open(repo_path(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
 
 def main(argv=sys.argv[1:]):
     arg_parser = argparse.ArgumentParser(description="My Git")
