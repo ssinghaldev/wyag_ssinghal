aa95e3fd728a7fd56bb454756d537e1be79e5ad2
Shubham Singhal <sbmsgl1129@gmail.com>
2019-07-04 16:58:17 +0530

    Add object_read dunc

--- libwyag.py
+++ libwyag.py
@@ -145,8 +145,38 @@ def repo_find(path='.', required=True):
 
     return repo_find(parent, required)
 
+def object_read(repo, sha):
+    """Read object object_id from git repository repo.
+       Return a GitObject whose exact type depends on the object"""
+    
+    path = repo_file(repo, "objects", sha[0:2], sha[2:])
 
-def main(argv=sys.argv[1:]):
+    with open(path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        # Read object type
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        #read and validate object size
+        y = raw.find(b'\x00', x)
+        size = int(raw[x:y].decode(ascii))
+        if size != len(raw) - y - 1:
+            raise Exception("Malformed object {0}: bad length".format(sha))
+        
+        if   fmt == b'commit' : c = GitCommit
+        elif fmt == b'tree'   : c = GitTree
+        elif fmt == b'tag'    : c = GitTag
+        elif fmt == b'blob'   : c = GitBlob
+        else:
+            raise Exception("Unknown type %s for object %s".format(fmt.decode(ascii),sha))
+        
+        return c(repo, raw[y+1:])
+
+def object_find(repo, name, fmt=None, follow=True):
+    return name
+
+ def main(argv=sys.argv[1:]):
     arg_parser = argparse.ArgumentParser(description="My Git")
 
     arg_sub_parsers = arg_parser.add_subparsers(title="Commands", dest="command")
